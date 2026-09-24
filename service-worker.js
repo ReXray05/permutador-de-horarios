bad3d87f6ae7361cab7a7e7f0ae8339e53cac943
ReXray05 <siulzenitram05@gmail.com>
2026-09-24 17:06:28 +0200

    Clear legacy caches and disable old service worker

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,27 @@
-const CACHE = 'planificador-v27';
-const INDEX = new URL('./index.html', self.registration.scope).href;
-self.addEventListener('install', event => event.waitUntil(
-  caches.open(CACHE).then(cache => cache.add(new Request(INDEX, {cache:'reload'}))).then(() => self.skipWaiting())
-));
-self.addEventListener('activate', event => event.waitUntil(
-  caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith('planificador-') && key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())
-));
-self.addEventListener('fetch', event => {
-  if(event.request.method !== 'GET' || event.request.mode !== 'navigate') return;
-  const url = new URL(event.request.url);
-  if(url.origin !== self.location.origin || !url.href.startsWith(self.registration.scope)) return;
-  event.respondWith(fetch(event.request, {cache:'no-store'}).then(response => {
-    if(response.ok){const copy=response.clone();event.waitUntil(caches.open(CACHE).then(cache=>cache.put(INDEX,copy)))}
-    return response;
-  }).catch(async()=> (await caches.match(INDEX)) || Response.error()));
+const CACHE_PREFIX = 'planificador-';
+
+self.addEventListener('install', event => {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(
+      keys
+        .filter(key => key.startsWith(CACHE_PREFIX))
+        .map(key => caches.delete(key))
+    );
+
+    await self.registration.unregister();
+
+    const windows = await self.clients.matchAll({
+      type: 'window',
+      includeUncontrolled: true
+    });
+
+    await Promise.all(
+      windows.map(client => client.navigate(client.url).catch(() => undefined))
+    );
+  })());
 });
